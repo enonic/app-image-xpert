6d0f73ab5cee09b97f71fc3be0c491f00d106f45
Glenn Ricaud <gri@enonic.com>
2016-04-15 11:29:04 +0200

    Image upload test

--- src/main/resources/site/parts/image-upload/image-upload.js
+++ src/main/resources/site/parts/image-upload/image-upload.js
@@ -1,17 +1,23 @@
 var portal = require('/lib/xp/portal');
-var thymeleaf = require('/lib/xp/thymeleaf');
+var mustacheLib = require('/lib/xp/mustache');
+var imageXpertLib = require('/lib/image-xpert');
 
 exports.get = function (req) {
     var imageCreationServiceUrl = portal.serviceUrl({service: "create-image"});
     var redirectPageId = portal.getContent()._id;
+    var categories = imageXpertLib.getCategories().
+        map(function (category) {
+            return {"name": category._name, "displayName": category.displayName};
+        });
 
     var params = {
         imageCreationServiceUrl: imageCreationServiceUrl,
-        redirectPageId: redirectPageId
+        redirectPageId: redirectPageId,
+        categories: categories
     };
 
     var view = resolve('image-upload.html');
-    var body = thymeleaf.render(view, params);
+    var body = mustacheLib.render(view, params);
 
     return {
         contentType: 'text/html',

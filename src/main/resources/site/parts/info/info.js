2f6be781d2fa544114421fae394cfe00748b4513
Glenn Ricaud <gri@enonic.com>
2016-04-18 23:06:04 +0200

    Info page

---
+++ src/main/resources/site/parts/info/info.js
@@ -0,0 +1,44 @@
+var contentLib = require('/lib/xp/content');
+var portalLib = require('/lib/xp/portal');
+var mustacheLib = require('/lib/xp/mustache');
+
+exports.get = function (req) {
+    var image = portalLib.getContent();
+
+    var binary = contentLib.get({
+        key: image.data.binary
+    });
+
+
+    var artist;
+    if (binary.data.artist) {
+        artist = binary.data.artist.toString();
+    }
+
+    var imageWidth = binary.x.media.imageInfo.imageWidth.toFixed(0);
+    var imageHeight = binary.x.media.imageInfo.imageHeight.toFixed(0);
+    var imageUrl = portalLib.imageUrl({
+        id: binary._id,
+        scale: "square(256)"
+    });
+
+
+    var view = resolve('info.html');
+    var body = mustacheLib.render(view, {
+        displayName: image.displayName,
+        createdTime: image.createdTime,
+        artist: artist,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+        imageUrl: imageUrl
+
+
+    });
+
+    return {
+        contentType: 'text/html',
+        body: body
+    };
+}
+;
+

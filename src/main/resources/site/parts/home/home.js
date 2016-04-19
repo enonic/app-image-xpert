5563890e25528daf83fa53bc816ef01b5d945035
Glenn Ricaud <gri@enonic.com>
2016-04-19 21:51:28 +0200

    Fix action on search field

--- src/main/resources/site/parts/home/home.js
+++ src/main/resources/site/parts/home/home.js
@@ -5,31 +5,33 @@ var imageXpertLib = require('/lib/image-xpert');
 exports.get = function (req) {
     var count = 0,
         categories = imageXpertLib.getCategories().
-        filter(function (category) {
-            return category.data.binaryImage;
-        }).
-        map(function (category) {
-            var imageUrl = portalLib.imageUrl({
-                id: category.data.binaryImage,
-                scale: "block(220,147)"
-            });
-            var linkUrl = imageXpertLib.generateGalleryPageUrl({
-                categoryId: category._id
-            });
+            filter(function (category) {
+                return category.data.binaryImage;
+            }).
+            map(function (category) {
+                var imageUrl = portalLib.imageUrl({
+                    id: category.data.binaryImage,
+                    scale: "block(220,147)"
+                });
+                var linkUrl = imageXpertLib.generateGalleryPageUrl({
+                    categoryId: category._id
+                });
                 count++;
-            return {
-                displayName: category.displayName.toLowerCase(),
-                imageUrl: imageUrl,
-                linkUrl: linkUrl,
-                newRow: (count == 4)
-            };
-        });
+                return {
+                    displayName: category.displayName.toLowerCase(),
+                    imageUrl: imageUrl,
+                    linkUrl: linkUrl,
+                    newRow: (count == 4)
+                };
+            });
 
+    var galleryPageUrl = imageXpertLib.generateGalleryPageUrl();
     var uploadPageUrl = imageXpertLib.generateUploadPageUrl();
 
     var view = resolve('home.html');
     var body = mustacheLib.render(view, {
         categories: categories,
+        galleryPageUrl: galleryPageUrl,
         uploadPageUrl: uploadPageUrl,
         assetUrl: portalLib.assetUrl('')
     });

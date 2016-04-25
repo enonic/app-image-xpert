3e4cdb57bd7e95e709e07b93e4d800af114e8fb4
Alan Semenov <ase@enonic.com>
2016-04-25 21:16:06 +0200

    Validation in the Upload form

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,4 +1,5 @@
 function toggleLayers() {
+    validateForm();
     document.querySelector(".main-container").classList.toggle("hidden");
     document.querySelector(".upload-container").classList.toggle("hidden");
 }
@@ -6,7 +7,18 @@ function previewImage(event) {
     var previewEl = document.querySelector('.upload-container .image-preview');
     previewEl.style.display = "block";
     previewEl.src = URL.createObjectURL(event.target.files[0]);
+
+    validateForm();
 }
 function onImageLoad(image) {
     image.style.height = "";
+}
+function validateForm() {
+    var categoryArr = Array.prototype.slice.call(document.querySelectorAll(".category-radio")),
+        categorySelected = categoryArr.some(function(categoryButton) {
+            return categoryButton.checked;
+        }),
+        fileSelected = document.querySelector(".file-name").value != "";
+
+    document.querySelector(".upload-button").disabled = (categorySelected && fileSelected) ? "" : "true";
 }

760845d4820725e2f3362d6c675a2d695aa34f80
Glenn Ricaud <gri@enonic.com>
2016-04-25 16:45:39 +0200

    Use attachment URL on download if untouched

--- src/main/resources/services/download-image/download-image.js
+++ src/main/resources/services/download-image/download-image.js
@@ -3,23 +3,32 @@ var imageXpertLib = require('/lib/image-xpert');
 
 exports.post = function (req) {
 
-    var scale;
-    if (req.params.size == "original") {
-        var binaryImage = imageXpertLib.getContentByKey(req.params.binary);
-        var width = binaryImage.x.media.imageInfo.imageWidth;
-        var height = binaryImage.x.media.imageInfo.imageHeight;
-        scale = "block(" + width.toFixed(0) + "," + height.toFixed(0) + ")";
+    var imageUrl;
+    if (req.params.size == "original" && req.params.format == "original") {
+        imageUrl = portalLib.attachmentUrl({
+            id: req.params.binary,
+            download: true
+        });
     } else {
-        var width = req.params.width;
-        var height = req.params.height;
-        scale = "block(" + width + "," + height + ")";
+        var scale;
+        if (req.params.size == "original") {
+            var binaryImage = imageXpertLib.getContentByKey(req.params.binary);
+            var width = binaryImage.x.media.imageInfo.imageWidth;
+            var height = binaryImage.x.media.imageInfo.imageHeight;
+            scale = "block(" + width.toFixed(0) + "," + height.toFixed(0) + ")";
+        } else {
+            var width = req.params.width;
+            var height = req.params.height;
+            scale = "block(" + width + "," + height + ")";
+        }
+
+        var imageUrl = portalLib.imageUrl({
+            id: req.params.binary,
+            scale: scale,
+            format: req.params.format == "custom" ? req.params.formatname : undefined
+        });
     }
 
-    var imageUrl = portalLib.imageUrl({
-        id: req.params.binary,
-        scale: scale,
-        format: req.params.format == "custom" ? req.params.formatname : undefined
-    });
 
     return {
         contentType: 'text/plain',

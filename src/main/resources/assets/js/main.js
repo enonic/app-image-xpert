604fa4975348a41934c2f197f5dd8fff4236b558
Alan Semenov <ase@enonic.com>
2016-05-27 16:59:38 +0200

    EXT-198 Main page - Create new album * Create a new album via dialog

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -22,9 +22,38 @@ function validateForm() {
 
     document.querySelector(".upload-button").disabled = (albumSelected && fileSelected) ? "" : "true";
 }
+
 function openFileUploadDialog() {
     var fileUploadEl = document.querySelector('input[name="file"]');
     if (fileUploadEl) {
+        //document.querySelector('.new-album-dialog-container .button-toolbar').classList.add("select-files");
+        closeNewAlbumDialog();
         fileUploadEl.click();
+    }
+    return false;
+}
+
+function openNewAlbumDialog() {
+    var albumNameTextBox = document.querySelector('input[name="albumName"]');
+    var createButton = document.querySelector('.button-create');
+
+    if (!!albumNameTextBox && !!createButton) {
+        albumNameTextBox.value = "";
+        createButton["disabled"] = true;
+    }
+    document.querySelector('.new-album-dialog-container').classList.add("visible");
+    document.querySelector('input[name="albumName"]').focus();
+}
+
+function closeNewAlbumDialog() {
+    document.querySelector('.new-album-dialog-container').classList.remove("visible");
+    document.querySelector('.new-album-dialog-container .button-toolbar').classList.remove("select-files");
+}
+
+function validateForm() {
+    var createButton = document.querySelector('.button-create');
+    var albumNameTextBox = document.querySelector('input[name="albumName"]');
+    if (!!createButton && !!albumNameTextBox) {
+        createButton["disabled"] = (albumNameTextBox.value.trim().length == 0);
     }
 }
